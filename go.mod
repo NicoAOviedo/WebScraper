3f71feff952a4d95f950c8280a96dfe1dcd9b458
Nicolas A. Oviedo <97.oviedo.nicolas@gmail.com>
2025-03-16 13:18:16 -0300

    scrapeo con status de un solo link

---
+++ go.mod
@@ -0,0 +1,7 @@
+module webScraper
+
+go 1.23.0
+
+toolchain go1.23.7
+
+require golang.org/x/net v0.37.0 // indirect
--- webScraper.go
+++ webScraper.go
@@ -1,27 +1,114 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"golang.org/x/net/html"
 )
 
-func main() {
-	statusCode,err := getStatusCode("https://x.com/home")
+var client = &http.Client{
+	Timeout: 5 * time.Second,
+}
+
+func getLinks(url string) ([]string, error) {
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("Error: ",err)
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var links []string
+	var extractLinks func(*html.Node)
+	extractLinks = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					absoluteURL := resolveURL(attr.Val, url)
+					links = append(links, absoluteURL)
+					break
+				}
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			extractLinks(c)
+		}
+	}
+	extractLinks(doc)
+
+	return links, nil
+}
+
+func resolveURL(href, baseURL string) string {
+	parsedBase, err := url.Parse(baseURL)
+	if err != nil {
+		return href
+	}
+
+	parsedHref, err := url.Parse(href)
+	if err != nil {
+		return href
+	}
+
+	return parsedBase.ResolveReference(parsedHref).String()
+}
+
+func getStatusCode(url string) (int, string) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, "Error en solicitud"
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		if os.IsTimeout(err) {
+			return 0, "Timeout"
+		}
+		return 0, "Error"
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode, http.StatusText(resp.StatusCode)
+}
+
+func main() {
+	var url string
+	fmt.Print("Ingrese su enlace a screapear: ")
+	fmt.Scanf("%s", &url)
+	links, err := getLinks(url)
+	if err != nil {
+		fmt.Println("Error:", err)
 		return
 	}
-	fmt.Println("Status Code: ", statusCode)
 
-}
+	fmt.Println("Enlaces encontrados:")
+	for _, link := range links {
 
-func getStatusCode(url string) (int, error) {
+		statusCode, statusText := getStatusCode(link)
+		// if err != nil {
+		// 	fmt.Println("Error: ", err)
+		// 	return
+		// }
 
-	res, err := http.Get(url)
-	if err != nil {
-		return 0, err
+		//if statusCode != http.StatusOK {
+		fmt.Println(link, "Code:", statusCode, statusText)
+		//}
 	}
-	defer res.Body.Close()
-	return res.StatusCode,nil
-}
 
+	fmt.Println("Scrapeo terminado.")
+}
